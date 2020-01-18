f4e161b99aff57a0931f0547044fd3da857d38bf
ASNordroid <n3tstrang3r@yandex.ru>
2020-01-18 23:52:17 +0300

    Update main.rs

--- Kmeans/src/main.rs
+++ Kmeans/src/main.rs
@@ -24,6 +24,13 @@ impl Distribution<PPoint> for Standard {
     }
 }
 
+// struct Kmeans {
+//     clcn = None let clcn = vec![vec![0.0; 6]; size];
+//     lbs = None let mut labels: Vec<u8> = vec![0; size];
+//     ncls = ncls
+//     tol = 0.0001
+// }
+
 // fn d2(p1: PPoint, p2: PPoint) -> f64 {
 //     return (p1.x - p2.x).powi(2) + (p1.y - p2.y).powi(2)
 // }
@@ -32,8 +39,39 @@ fn to_list(ps: Vec<PPoint>) -> Vec<(f64, f64)> {
     return ps.into_iter().map(|p| {(p.x, p.y)}).collect()
 }
 
-// fn predict() -> &[PPoint] {
-// }
+fn norm(x: Vec<f64>) -> f64 {
+    return x.iter().map(|i| i * i).sum::<f64>().sqrt();
+}
+
+fn sub(a: Vec<u32>, b: Vec<f64>) -> Vec<f64> {
+    let mut z: Vec<f64> = vec![0.0];
+    for (i, (aval, bval)) in a.iter().zip(&b).enumerate() {
+        z[i] = *aval as f64 - bval;
+    }
+    z
+}
+
+fn predict(x: Vec<Vec<u32>>, ncls: u32) -> Vec<u32> {
+    let size = x.len();
+    let clcn = vec![vec![0.0; 6]; size];
+    let mut labels: Vec<u32> = vec![0; size];
+    let mut md: Vec<f64> = Vec::new();
+    for i in 0..size {
+        md[i] = norm(sub(x[i], clcn[0]));
+    }
+    
+    for cl in 1..ncls {
+        for i in 0..size {
+            let dist = 1.0; // norm(X[i] - self.clcn[cl])
+            if dist < md[i] {
+                md[i]     = dist;
+                labels[i] = cl;
+            }
+        }
+    }
+
+    return labels;
+}
 
 // fn fit(n: i32, data: &[PPoint]) {
 //     let clasters =
@@ -45,6 +83,8 @@ fn main() {
     for _ in 0..100 {
         arr.push(rng.gen());
     }
+
+
 
     let s: Scatter = Scatter::from_slice(&to_list(arr)[..]).style(&Style::new()
         .marker(Marker::Square)
