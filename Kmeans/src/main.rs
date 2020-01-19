4cfe2c522663db8b35c40ff100ad21b1ae4c4b64
ASNordroid <n3tstrang3r@yandex.ru>
2020-01-19 23:23:49 +0300

    Start working on predict in Kmeans

--- Kmeans/src/main.rs
+++ Kmeans/src/main.rs
@@ -3,6 +3,7 @@ extern crate plotlib;
 
 use rand::Rng;
 use rand::distributions::{Distribution, Standard};
+use rand::seq::SliceRandom;
 use plotlib::page::Page;
 use plotlib::scatter::{Scatter, Style};
 use plotlib::style::{Marker, Point};
@@ -24,23 +25,23 @@ impl Distribution<PPoint> for Standard {
     }
 }
 
-// struct Kmeans {
-//     clcn = None let clcn = vec![vec![0.0; 6]; size];
-//     lbs = None let mut labels: Vec<u8> = vec![0; size];
-//     ncls = ncls
-//     tol = 0.0001
-// }
+struct Kmeans {
+    clcn: Vec<f64>, // = None let clcn = vec![vec![0.0; 6]; size];
+    lbs: Vec<u8>, // None let mut labels: Vec<u8> = vec![0; size];
+    ncls: u8,
+    tol: f64
+}
 
 // fn d2(p1: PPoint, p2: PPoint) -> f64 {
 //     return (p1.x - p2.x).powi(2) + (p1.y - p2.y).powi(2)
 // }
 
 fn to_list(ps: Vec<PPoint>) -> Vec<(f64, f64)> {
-    return ps.into_iter().map(|p| {(p.x, p.y)}).collect()
+    ps.into_iter().map(|p| {(p.x, p.y)}).collect()
 }
 
 fn norm(x: Vec<f64>) -> f64 {
-    return x.iter().map(|i| i * i).sum::<f64>().sqrt();
+    x.iter().map(|i| i * i).sum::<f64>().sqrt()
 }
 
 fn sub(a: Vec<u32>, b: Vec<f64>) -> Vec<f64> {
@@ -51,18 +52,18 @@ fn sub(a: Vec<u32>, b: Vec<f64>) -> Vec<f64> {
     z
 }
 
-fn predict(x: Vec<Vec<u32>>, ncls: u32) -> Vec<u32> {
+fn predict(km: Kmeans, x: Vec<Vec<u32>>) -> Vec<u8> {
     let size = x.len();
-    let clcn = vec![vec![0.0; 6]; size];
-    let mut labels: Vec<u32> = vec![0; size];
+    let clcn = vec![vec![0.0; 6]; size]; // TODO: struct
+    let mut labels: Vec<u8> = vec![0; size];
     let mut md: Vec<f64> = Vec::new();
     for i in 0..size {
-        md[i] = norm(sub(x[i], clcn[0]));
+        md[i] = norm(sub(x[i].clone(), clcn[0].clone())); // TODO: struct
     }
     
-    for cl in 1..ncls {
+    for cl in 1..km.ncls {
         for i in 0..size {
-            let dist = 1.0; // norm(X[i] - self.clcn[cl])
+            let dist = norm(sub(x[i].clone(), clcn[cl as usize].clone())); // TODO: struct
             if dist < md[i] {
                 md[i]     = dist;
                 labels[i] = cl;
@@ -73,9 +74,37 @@ fn predict(x: Vec<Vec<u32>>, ncls: u32) -> Vec<u32> {
     return labels;
 }
 
-// fn fit(n: i32, data: &[PPoint]) {
-//     let clasters =
-// }
+fn fit(mut km: Kmeans, x: Vec<Vec<u32>>) -> Kmeans {
+    let mut rng = &mut rand::thread_rng();
+    let smp = x.len();
+    let clcn_n: Vec<i32> = (0..smp as i32).collect::<Vec<i32>>().as_slice().choose_multiple(&mut rng, km.ncls as usize).cloned().collect();
+    println!("{:?}", clcn_n);
+    km.lbs = vec![0; smp];
+    let mut md = vec![0.0; smp];
+
+    while true {
+        for i in 0..smp {
+            md[i] = 1.0; //norm(sub(x[i].clone(), clcn[0].clone()));
+            km.lbs[i] = 0
+        }
+
+        for cl in 1..km.ncls {
+            for i in 0..smp {
+                let dist = 1.0; //norm(sub(x[i].clone(), clcn[cl].clone()));
+                if dist < md[i] {
+                    md[i] = dist;
+                    km.lbs[i] = cl
+                }
+            }
+        }
+
+        //nc = np.array([X[self.lbs == i].sum(axis=0) / X[self.lbs == i].shape[0] for i in range(self.ncls)])
+        //if (np.abs(nc - self.clcn) < self.tol).all(): break
+        //self.clcn = nc.copy()
+    }
+
+    return km
+}
 
 fn main() {
     let mut rng = rand::thread_rng();
@@ -84,7 +113,22 @@ fn main() {
         arr.push(rng.gen());
     }
 
+    let mut km = Kmeans {
+        clcn: Vec::new(),
+        lbs: Vec::new(),
+        ncls: 3,
+        tol: 0.001
+    };
 
+    fit(km, vec![vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86],
+                vec![1, 2, 100, 12, 43, 463, 25, 42, 6, 86]]);
 
     let s: Scatter = Scatter::from_slice(&to_list(arr)[..]).style(&Style::new()
         .marker(Marker::Square)
