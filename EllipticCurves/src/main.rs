70b61eb6650eda2ebeddcceb011afa0a980045cf
ASNordroid <n3tstrang3r@yandex.ru>
2020-09-03 02:10:20 +0300

    Add extended_euclidean_algorithm to EllipticCurves

--- EllipticCurves/src/main.rs
+++ EllipticCurves/src/main.rs
@@ -1,11 +1,10 @@
-extern crate num_bigint;
-extern crate num_traits;
-use num_bigint::{BigUint, ToBigUint};
-// use num_traits::{Zero, One};
+use num_bigint::{BigUint, ToBigUint, BigInt, ToBigInt};
+use num_traits::{Zero, One};
 // use std::mem::replace;
 
 fn elliptic_curve(x: BigUint, y: BigUint, p: BigUint) -> bool
 {
+    // ! NOTDONE
     let A = BigUint::parse_bytes(b"2060348749492490789823288813930625381761", 10).unwrap();
     let B = BigUint::parse_bytes(b"1991637506671384833171818673149062805973", 10).unwrap();
 
@@ -16,20 +15,24 @@ fn elliptic_curve(x: BigUint, y: BigUint, p: BigUint) -> bool
     // (BigUint::modpow(&x, &ToBigUint::to_biguint(&3).unwrap(), &ToBigUint::to_biguint(&1).unwrap()) + (A % p.clone()) * x + (B % p.clone())) % p.clone();
 }
 
-fn extended_euclidean_algorithm(a: BigUint, b: BigUint) -> (BigUint, BigUint, BigUint)
+fn extended_euclidean_algorithm(a: BigUint, b: BigUint) -> (BigInt, BigInt, BigInt)
 {
-    let (mut s, mut old_s) = (ToBigUint::to_biguint(&0).unwrap(), ToBigUint::to_biguint(&1).unwrap());
-    let (mut t, mut old_t) = (ToBigUint::to_biguint(&1).unwrap(), ToBigUint::to_biguint(&0).unwrap());
-    let (mut r, mut old_r) = (b, a);
-
-    while r != ToBigUint::to_biguint(&0).unwrap() {
-        let quotient = old_r / r;
-        old_r = r;
-        r = old_r - quotient * r;
-        old_s = s;
-        s = old_s - quotient * s;
-        old_t = t;
-        t = old_t - quotient * t;
+    let (mut s, mut old_s): (BigInt, BigInt) = (Zero::zero(), One::one());
+    let (mut t, mut old_t): (BigInt, BigInt) = (One::one(), Zero::zero());
+    let (mut r, mut old_r) = (b.to_bigint().unwrap(), a.to_bigint().unwrap());
+    let mut j;
+    while r != Zero::zero() {
+        // TODO: wtf is this
+        let quotient = old_r.clone() / r.clone();
+        j = old_r.clone();
+        old_r = r.clone();
+        r = j.clone() - quotient.clone() * r;
+        j = old_s.clone();
+        old_s = s.clone();
+        s = j.clone() - quotient.clone() * s;
+        j = old_t.clone();
+        old_t = t.clone();
+        t = j.clone() - quotient * t;
     }
 
     return (old_r, old_s, old_t);
