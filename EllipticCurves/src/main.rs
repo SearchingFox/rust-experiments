8e8f7720862dcc780717bedccf39ef38968a7af9
ASNordroid <n3tstrang3r@yandex.ru>
2020-09-05 23:52:28 +0300

    Massive update of EllipticCurves

--- EllipticCurves/src/main.rs
+++ EllipticCurves/src/main.rs
@@ -1,99 +1,111 @@
-use num_bigint::{BigUint, ToBigUint, BigInt, ToBigInt};
-use num_traits::{Zero, One};
+#![feature(try_trait)]
+use num_bigint::{BigInt, ToBigInt};
+use num_traits::{One, Zero};
 use std::time::Instant;
-// use std::mem::replace;
 
-fn in_elliptic_curve(x: BigUint, y: BigUint, p: BigUint) -> bool {
-    let a = BigUint::parse_bytes(b"2060348749492490789823288813930625381761", 10).unwrap();
-    let b = BigUint::parse_bytes(b"1991637506671384833171818673149062805973", 10).unwrap();
-    return BigUint::modpow(&y, &2.to_biguint().unwrap(), &p) == (&x * &x * &x + (a % &p) * &x + (b % &p)) % &p
+const C1: &[u8; 40] = b"1860348749492490789823288813930625381760";
+const C2: &[u8; 40] = b"2001637506671384833171818673149062805974";
+
+fn in_elliptic_curve(x: BigInt, y: BigInt, p: BigInt) -> bool {
+    let a = BigInt::parse_bytes(C1, 10).unwrap();
+    let b = BigInt::parse_bytes(C2, 10).unwrap();
+
+    BigInt::modpow(&y, &2.to_bigint().unwrap(), &p)
+        == (&x * &x * &x + (a % &p) * &x + (b % &p)) % &p
 }
 
-fn extended_euclidean_algorithm(a: BigUint, b: BigUint) -> (BigInt, BigInt, BigInt)
-{
+fn extended_euclidean_algorithm(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
     let (mut s, mut old_s): (BigInt, BigInt) = (Zero::zero(), One::one());
     let (mut t, mut old_t): (BigInt, BigInt) = (One::one(), Zero::zero());
-    let (mut r, mut old_r) = (b.to_bigint().unwrap(), a.to_bigint().unwrap());
-    let mut j;
+    let (mut r, mut old_r) = (b, a);
+
     while r != Zero::zero() {
-        // TODO: wtf is this
-        let quotient = old_r.clone() / r.clone();
-        j = old_r.clone();
+        let quotient = &old_r / &r;
+        let tmp = old_r.clone();
         old_r = r.clone();
-        r = j.clone() - quotient.clone() * r;
-        j = old_s.clone();
+        r = tmp - &quotient * r;
+        let tmp = old_s.clone();
         old_s = s.clone();
-        s = j.clone() - quotient.clone() * s;
-        j = old_t.clone();
+        s = tmp - &quotient * s;
+        let tmp = old_t.clone();
         old_t = t.clone();
-        t = j.clone() - quotient * t;
+        t = tmp - quotient * t;
     }
 
-    return (old_r, old_s, old_t);
+    (old_r, old_s, old_t)
 }
 
-fn inverse_of(n: BigUint, p: BigUint) -> BigInt
-{
+fn inverse_of(n: BigInt, p: BigInt) -> BigInt {
     let (gcd, x, y) = extended_euclidean_algorithm(n.clone(), p.clone());
-    assert!((n.to_bigint().unwrap() * &x + p.to_bigint().unwrap() * y) % p.to_bigint().unwrap() == gcd);
+    assert!(
+        (&n * &x + &p * y) % &p == gcd
+    );
 
     if gcd != 1.to_bigint().unwrap() {
         panic!("{} has no multiplicative inverse modulo {}", n, p);
     } else {
-        return x % p.to_bigint().unwrap()
+        return x % p;
     }
 }
 
-fn add_points((x1, x2): (BigInt, BigInt), (y1, y2): (BigInt, BigInt), p: BigInt) -> (BigInt, BigInt)
-{
-    let a = BigInt::parse_bytes(b"2060348749492490789823288813930625381761", 10).unwrap();
+fn add_points(
+    (x1, x2): (BigInt, BigInt),
+    (y1, y2): (BigInt, BigInt),
+    p: BigInt
+) -> (BigInt, BigInt) {
     if (&x1, &x2) == (&Zero::zero(), &Zero::zero()) {
-        return (y1.to_bigint().unwrap(), y2.to_bigint().unwrap())
+        return (y1.to_bigint().unwrap(), y2.to_bigint().unwrap());
     } else if (&y1, &y2) == (&Zero::zero(), &Zero::zero()) {
-        return (x1.to_bigint().unwrap(), x2.to_bigint().unwrap())
+        return (x1.to_bigint().unwrap(), x2.to_bigint().unwrap());
     } else if &x1 == &y1 && &x2 != &y2 {
-        return (Zero::zero(), Zero::zero())
+        return (Zero::zero(), Zero::zero());
     }
 
+    let a = BigInt::parse_bytes(C1, 10).unwrap();
     let s = if (&x1, &x2) == (&y1, &y2) {
-        ((3.to_bigint().unwrap() * &x1 * &x1 + (a % &p)) * inverse_of((2.to_bigint().unwrap() * &x2).to_biguint().unwrap(), p.to_biguint().unwrap())) % &p
+        ((3.to_bigint().unwrap() * &x1 * &x1 + (a % &p))
+            * inverse_of(2.to_bigint().unwrap() * &x2, p.clone())) % &p
     } else {
-         ((&x2 - &y2) * inverse_of((&x1 - &y1).to_biguint().unwrap(), p.to_biguint().unwrap())) % &p
+        ((&x2 - &y2) * inverse_of(&x1 - &y1, p.clone())) % &p
     };
     let x = (&s * &s - 2.to_bigint().unwrap() * &x1) % &p;
     let y = (&x2 + s * (&x - &x1)) % &p;
-    return (x, -y % p)
+
+    (x, -y % p)
 }
 
-fn order_point(point: &(BigInt, BigInt), p: BigInt) -> i64
-{
-    let mut i = 1;
+fn point_order(point: &(BigInt, BigInt), p: BigInt) -> i32 {
+    let mut count = 1;
     let mut check = add_points(point.clone(), point.clone(), p.clone());
     while check != (Zero::zero(), Zero::zero()) {
         check = add_points(check, point.clone(), p.clone());
-        i += 1;
+        count += 1;
     }
-    return i
+
+    count
 }
 
-fn main() {
+fn main() -> Result<(), std::option::NoneError> {
     let p = 250; // 13
     let mut points: Vec<(BigInt, BigInt)> = Vec::new();
     let start = Instant::now();
 
     for i in 0..p {
         for j in 0..p {
-            if in_elliptic_curve(i.to_biguint().unwrap()
-                               , j.to_biguint().unwrap()
-                               , p.to_biguint().unwrap()) {
-                points.push((i.to_bigint().unwrap()
-                           , j.to_bigint().unwrap()));
+            if in_elliptic_curve(i.to_bigint()?, j.to_bigint()?, p.to_bigint()?) {
+                points.push((i.to_bigint()?, j.to_bigint()?));
             }
         }
     }
-    
+
+    let a = BigInt::parse_bytes(C1, 10).unwrap();
+    let b = BigInt::parse_bytes(C2, 10).unwrap();
+    println!("y^2 = x^3 + {} * x + {} (mod {})", a % p.to_bigint().unwrap(), b % p.to_bigint().unwrap(), p);
+
     println!("Curve order = {}", &points.len());
-    let point = &points[10];
-    println!("Point order {:?}: {:?}", point, order_point(point, p.to_bigint().unwrap()));
+    // println!("{:?}", points);
+    let point = &points[0];
+    println!("Point order {:?}: {:?}", point, point_order(point, p.to_bigint().unwrap()));
     println!("Time elapsed is: {:?}", start.elapsed());
+    Ok(())
 }
