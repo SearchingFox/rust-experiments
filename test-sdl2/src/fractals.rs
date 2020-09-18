03cbb471bb26db401650502810de07419efe75ca
ASNordroid <n3tstrang3r@yandex.ru>
2020-09-18 06:01:03 +0300

    Add threads to fractals.rs in test-sdl2 and other small changes

--- test-sdl2/src/fractals.rs
+++ test-sdl2/src/fractals.rs
@@ -1,16 +1,17 @@
-#![allow(dead_code)]
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::{Color, PixelFormatEnum};
+use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Canvas, Texture};
 use sdl2::surface::Surface;
 use sdl2::video::Window;
 use std::error::Error;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
 
-const WINDOW_WIDTH: u32  = 1900; //2500
-const WINDOW_HEIGHT: u32 = 1060;
+const WINDOW_WIDTH:  u32 = 1920;
+const WINDOW_HEIGHT: u32 = 1080;
 
 type Complex = (f64, f64);
 
@@ -18,41 +19,41 @@ fn mandelbrot((a, b): Complex) -> Color {
     let mut t = (0.0, 0.0);
     for i in 0..1000 {
         t = (t.0*t.0 - t.1*t.1 + a, 2.0*t.0*t.1 + b);
-        if f64::sqrt(t.0*t.0 + t.1*t.1) > 4.0 {
+        if f64::sqrt(t.0*t.0 + t.1*t.1) > 4.0 { // try without sqrt
             return Color::RGB((i % 255) as u8, (i % 255) as u8, (i * 10 % 255) as u8)
             // return Color::RGB(i * 20 % 255, i * 20 % 255, i * 20 % 255);
         }
     }
-    Color::RGB(0, 0, 0)
+    Color::BLACK
 }
 
-fn what((a, b): Complex) -> Color {
+fn broken_mandelbrot((a, b): Complex) -> Color {
     let (mut x, mut y) = (0.0, 0.0);
-    for _ in 0..100 {
+    for i in 0..1000 {
         x = x * x - y * y + a;
-        y = 2.0 * x * y + b;
+        y = 2.0 * x * y + b;  // wrong x
         if f64::sqrt(x * x + y * y) > 2.0 {
-            return Color::RGB(255, 255, 255);
+            return Color::RGB((i * 20 % 255) as u8, (i * 20 % 255) as u8, (i * 20 % 255) as u8);
         }
     }
-    Color::RGB(0, 0, 0)
+    Color::BLACK
 }
 
-fn burning_ship((a, b): Complex) -> Color {
-    let mut zx = a + 0.5;
-    let mut zy = b;
-    let mut iter = 0;
-    let mi: u32 = 1000;
-    while (zx*zx + zy*zy < 4.0 && iter < mi) {
-        let xtemp = zx*zx - zy*zy + a;
-        zy = f64::abs(2.0*zx*zy + b);
-        zx = f64::abs(xtemp);
-        iter += 1;
+fn burning_ship((mut a, mut b): Complex) -> Color {
+    a += -17.;
+    b += -0.5;
+    let s = 10.;
+    a /= s;
+    b /= s;
+
+    let mut z = (0.0, 0.0);
+    for i in 0..1000 {
+        z = (z.0*z.0 - z.1*z.1 + a, 2.0 * f64::abs(z.0*z.1) + b);
+        if z.0*z.0 + z.1*z.1 > 4.0 {
+            return Color::RGB(100 + (i % 155) as u8, 30 + (i % 100) as u8, 0)
+        }
     }
-    if iter == mi { // Belongs to the set
-        return Color::RGB(100, 0, 0);
-    }
-    return Color::RGB(220, (iter * 10 % 255) as u8, (iter * 10 % 255) as u8);
+    Color::BLACK
 }
 
 fn to_compl_plain(x: u32, y: u32) -> Complex {
@@ -60,27 +61,38 @@ fn to_compl_plain(x: u32, y: u32) -> Complex {
             (y as f64 - WINDOW_HEIGHT as f64 / 2.) * 2. / WINDOW_HEIGHT as f64)
 }
 
-fn to_window((a, b): Complex) -> (u32, u32) {
-    return ((a as f64 * WINDOW_WIDTH as f64 / 3.4 + WINDOW_WIDTH as f64 / 2.) as u32,
-            (b as f64 * WINDOW_HEIGHT as f64 / 2. - WINDOW_HEIGHT as f64 / 2.) as u32)
-}
-
 pub fn main_fractals() -> Result<(), Box<dyn Error>> {
+    let temp_cache: Arc<Mutex<Vec<(u32, Vec<Color>)>>> = Arc::new(Mutex::new(vec![]));
+    let mut threads = vec![];
+    for t_num in 0..5 {
+        threads.push(thread::spawn({
+            let cln = Arc::clone(&temp_cache);
+            move || {
+                let mut v = cln.lock().unwrap();
+                let a = (WINDOW_WIDTH / 5) as u32;
+                for i in a*t_num..a*(t_num+1) {
+                    let mut temp = Vec::<Color>::with_capacity(WINDOW_HEIGHT as usize);
+                    for j in 0..WINDOW_HEIGHT {
+                        temp.push(broken_mandelbrot(to_compl_plain(i, j)));
+                    }
+                    v.push((i, temp));
+                }
+            }
+        }));
+    }
+    for t in threads {
+        t.join().unwrap();
+    }
+
+    let mut cache = Arc::try_unwrap(temp_cache).unwrap().into_inner().unwrap();
+    cache.sort_by(|(x, _), (y, _)| x.cmp(y));
+
     let sdl_context = sdl2::init()?;
     let window = sdl_context.video()?
         .window("Fractals", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()?;
-
     let mut canvas = window.into_canvas().build()?;
-
-    let mut cache: Vec<Vec<Color>> = vec![vec![Color::RGB(0, 0, 0); WINDOW_HEIGHT as usize]; WINDOW_WIDTH as usize];
-    for i in 0..WINDOW_WIDTH {
-        for j in 0..WINDOW_HEIGHT {
-            // ? maybe change transition to from complex plane to screen
-            cache[i as usize][j as usize] = burning_ship(to_compl_plain(i, j));
-        }
-    }
 
     let mut event_pump = sdl_context.event_pump()?;
     'running: loop {
@@ -95,7 +107,7 @@ pub fn main_fractals() -> Result<(), Box<dyn Error>> {
         for i in 0..WINDOW_WIDTH {
             for j in 0..WINDOW_HEIGHT {
                 let t: Point = Point::new(i as i32, j as i32);
-                canvas.set_draw_color(cache[i as usize][j as usize]);
+                canvas.set_draw_color(cache[i as usize].1[j as usize]);
                 canvas.draw_point(t)?;
             }
         }
--- test-sdl2/src/main.rs
+++ test-sdl2/src/main.rs
@@ -1,11 +1,15 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
 
-mod geometry;
 mod fractals;
+mod geometry;
 mod gol;
+mod ifs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // fractals::main_fractals()
+    fractals::main_fractals()
+    // geometry::main_geometry()
     // gol::main_gol()
-    geometry::main_geometry()
+    // ifs::main_ifs()
 }
