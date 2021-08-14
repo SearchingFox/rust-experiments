50a8f760cb48041e099a3c53cb648a84082667e1
SearchingFox <n3tstrang3r@yandex.ru>
2021-08-15 01:09:18 +0300

    Update firefox-bookmarks

--- BookmarksFirefox/src/main.rs
+++ BookmarksFirefox/src/main.rs
@@ -1,16 +1,5 @@
-#![feature(try_trait)]
 use std::fs;
 use std::path::Path;
-
-fn deduplicate(inp: &Vec<String>) -> Vec<String> {
-    let mut result: Vec<String> = Vec::with_capacity(inp.len());
-    for i in inp {
-        if !result.contains(i) {
-            result.push(i.to_string());
-        }
-    }
-    result
-}
 
 fn del_http(url: String) -> String {
     if url.starts_with("http") {
@@ -19,6 +8,7 @@ fn del_http(url: String) -> String {
             return t[1].to_string()
         }
     }
+
     url
 }
 
@@ -26,62 +16,58 @@ fn get_from_html(file_path: &Path) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
     for l in fs::read_to_string(file_path).unwrap().lines() {
         if l.contains("<A") {
-            result.push(del_http(l[l.find("\"").unwrap()+1..l.find("\" A").unwrap()].to_string()));
+            result.push(del_http(l[l.find('\"').unwrap()+1..l.find("\" A").unwrap()].to_string()));
         }
     }
     result
 }
 
 fn get_from_folder(folder_path: &Path) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
-    for file in fs::read_dir(folder_path).unwrap() {
-        result.extend( // ? append
-            fs::read_to_string(file.unwrap().path())
-            .unwrap()
-            .split("\n")
-            // .filter(|i| i.starts_with("http"))
-            .map(|i| del_http(i.to_string()))
-            .collect::<Vec<_>>());
-    }
-    deduplicate(&result)
+    let result = fs::read_dir(folder_path).unwrap().map(|file| {
+        fs::read_to_string(file.unwrap().path())
+        .unwrap()
+        .lines()
+        .map(|i| del_http(i.to_string()))
+        .collect::<Vec<_>>()
+    }).collect::<Vec<_>>().concat();
+    
+    result
 }
 
-fn del_existing(all_urls: &Vec<String>, test_urls: &Vec<String>, format: &str) -> Vec<String> {
+// ? &[] or &Vec<String>
+fn del_existing(all_urls: &[String], test_urls: &[String], format: &str) -> Vec<String> {
     let mut result = Vec::new();
-    if format == "new" {
-        for (i, j) in (&test_urls).iter().enumerate() {
-            if i % 2 == 1 && !all_urls.contains(&del_http(j.to_string())) {
-                result.push(j.to_string());  // ? or .clone()
-            }
-        }
-    } else if format == "old" {
-        for i in test_urls {
-            if !all_urls.contains(&del_http(i.to_string())) {
-                result.push(i.to_string());
-            }
+    let step = match format {
+        "new" => 2, // check if number of lines is even
+        "old" => 1,
+        _ => panic!("Invalid format")
+    };
+    for url in test_urls.iter().skip(step-1).step_by(step) {
+        if !all_urls.contains(&del_http(url.to_string()))
+        && !result.contains(url) {
+            result.push(url.to_string());  // ? or .clone()
         }
     }
-    deduplicate(&result)
+
+    result
 }
 
-fn main() -> Result<(), Box<std::option::NoneError>> {
-    // let arg = std::env::args().nth(1).expect("no file given");
-    // let source_path = Path::new(&arg);
-    let source_path = Path::new(r#""#);
-    let source = fs::read_to_string(source_path).unwrap().split("\n").map(String::from).collect::<Vec<_>>();
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    let format = &args[1];
+    let source_path = Path::new(&args[2]);
+    let source = fs::read_to_string(source_path).unwrap().lines().map(String::from).collect::<Vec<_>>();
 
-    let bookmarks_path = Path::new(r#"C:\Users\Asus\Desktop\bookmarks_firefox_200723_0329_noicons.html"#);
-    let mut all_urls = get_from_folder(Path::new(r#"C:\Users\Asus\Desktop\firefox_resolve\tabs"#));
-    all_urls.extend(get_from_html(bookmarks_path));
+    let all_urls = [
+    ].concat();
 
-    let result = del_existing(&all_urls, &source, "new");
-    println!("notin bookmarks: {}", result.len());
+    let result = del_existing(&all_urls, &source, format);
+    println!("{} links are not in bookmarks", result.len());
 
-    fs::write(
-        source_path.parent()?.join(
-            source_path.file_stem()?.to_str()?.to_string() + "_uniq_rusted.txt"
-        ), result.join("\n")
-    ).expect("Unable write to file");
-
-    Ok(())
+    if result.len() > 0 {
+        fs::write(
+            source_path.parent().unwrap().join(
+                source_path.file_stem().unwrap().to_str().unwrap().to_string() + "_uniq_rusted.txt"
+            ), result.join("\n")).expect("Unable write to output file");
+    }
 }
