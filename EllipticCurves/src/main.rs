6d03128f3dccf4fd9331f911a00c43066b4680c8
SearchingFox <n3tstrang3r@yandex.ru>
2021-08-21 23:54:13 +0300

    Switch elliptic-curves from num_bigint to ramp

--- EllipticCurves/src/main.rs
+++ EllipticCurves/src/main.rs
@@ -1,25 +1,26 @@
-#![feature(try_trait)]
-use num_bigint::{BigInt, ToBigInt};
-use num_traits::{One, Zero};
+// #![feature(try_trait)]
+// use num_Int::{Int, ToInt};
+// use num_traits::{One, zero()};
+use ramp::int::Int;
 use std::time::Instant;
 
-const C1: &[u8; 40] = b"1860348749492490789823288813930625381760";
-const C2: &[u8; 40] = b"2001637506671384833171818673149062805974";
+const C1: &str = "1860348749492490789823288813930625381760";
+const C2: &str = "2001637506671384833171818673149062805974";
 
-fn in_elliptic_curve(x: BigInt, y: BigInt, p: BigInt) -> bool {
-    let a = BigInt::parse_bytes(C1, 10).unwrap();
-    let b = BigInt::parse_bytes(C2, 10).unwrap();
+fn in_elliptic_curve(x: Int, y: Int, p: Int) -> bool {
+    let a = Int::from_str_radix(C1, 10).unwrap();
+    let b = Int::from_str_radix(C2, 10).unwrap();
 
-    BigInt::modpow(&y, &2.to_bigint().unwrap(), &p)
+    Int::pow_mod(&y, &Int::from(2), &p)
         == (&x * &x * &x + (a % &p) * &x + (b % &p)) % &p
 }
 
-fn extended_euclidean_algorithm(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
-    let (mut s, mut old_s): (BigInt, BigInt) = (Zero::zero(), One::one());
-    let (mut t, mut old_t): (BigInt, BigInt) = (One::one(), Zero::zero());
+fn extended_euclidean_algorithm(a: Int, b: Int) -> (Int, Int, Int) {
+    let (mut s, mut old_s): (Int, Int) = (Int::zero(), Int::one());
+    let (mut t, mut old_t): (Int, Int) = (Int::one(), Int::zero());
     let (mut r, mut old_r) = (b, a);
 
-    while r != Zero::zero() {
+    while r != Int::zero() {
         let quotient = &old_r / &r;
         let tmp = old_r.clone();
         old_r = r.clone();
@@ -35,49 +36,49 @@ fn extended_euclidean_algorithm(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt
     (old_r, old_s, old_t)
 }
 
-fn inverse_of(n: BigInt, p: BigInt) -> BigInt {
+fn inverse_of(n: Int, p: Int) -> Int {
     let (gcd, x, y) = extended_euclidean_algorithm(n.clone(), p.clone());
     assert!(
         (&n * &x + &p * y) % &p == gcd
     );
 
-    if gcd != 1.to_bigint().unwrap() {
+    if gcd != Int::one() {
         panic!("{} has no multiplicative inverse modulo {}", n, p);
     } else {
-        return x % p;
+        x % p
     }
 }
 
 fn add_points(
-    (x1, x2): (BigInt, BigInt),
-    (y1, y2): (BigInt, BigInt),
-    p: BigInt
-) -> (BigInt, BigInt) {
-    if (&x1, &x2) == (&Zero::zero(), &Zero::zero()) {
-        return (y1.to_bigint().unwrap(), y2.to_bigint().unwrap());
-    } else if (&y1, &y2) == (&Zero::zero(), &Zero::zero()) {
-        return (x1.to_bigint().unwrap(), x2.to_bigint().unwrap());
-    } else if &x1 == &y1 && &x2 != &y2 {
-        return (Zero::zero(), Zero::zero());
+    (x1, x2): (Int, Int),
+    (y1, y2): (Int, Int),
+    p: Int
+) -> (Int, Int) {
+    if (&x1, &x2) == (&Int::zero(), &Int::zero()) {
+        return (y1, y2);
+    } else if (&y1, &y2) == (&Int::zero(), &Int::zero()) {
+        return (x1, x2);
+    } else if x1 == y1 && x2 != y2 {
+        return (Int::zero(), Int::zero());
     }
 
-    let a = BigInt::parse_bytes(C1, 10).unwrap();
+    let a = Int::from_str_radix(C1, 10).unwrap();
     let s = if (&x1, &x2) == (&y1, &y2) {
-        ((3.to_bigint().unwrap() * &x1 * &x1 + (a % &p))
-            * inverse_of(2.to_bigint().unwrap() * &x2, p.clone())) % &p
+        ((Int::from(3) * &x1 * &x1 + (a % &p))
+            * inverse_of(Int::from(2) * &x2, p.clone())) % &p
     } else {
         ((&x2 - &y2) * inverse_of(&x1 - &y1, p.clone())) % &p
     };
-    let x = (&s * &s - 2.to_bigint().unwrap() * &x1) % &p;
+    let x = (&s * &s - Int::from(2) * &x1) % &p;
     let y = (&x2 + s * (&x - &x1)) % &p;
 
     (x, -y % p)
 }
 
-fn point_order(point: &(BigInt, BigInt), p: BigInt) -> i32 {
+fn point_order(point: &(Int, Int), p: Int) -> i32 {
     let mut count = 1;
     let mut check = add_points(point.clone(), point.clone(), p.clone());
-    while check != (Zero::zero(), Zero::zero()) {
+    while check != (Int::zero(), Int::zero()) {
         check = add_points(check, point.clone(), p.clone());
         count += 1;
     }
@@ -85,27 +86,26 @@ fn point_order(point: &(BigInt, BigInt), p: BigInt) -> i32 {
     count
 }
 
-fn main() -> Result<(), std::option::NoneError> {
-    let p = 250; // 13
-    let mut points: Vec<(BigInt, BigInt)> = Vec::new();
+fn main() {
+    let p = 2501; // 3
+    let mut points: Vec<(Int, Int)> = Vec::new();
     let start = Instant::now();
 
     for i in 0..p {
         for j in 0..p {
-            if in_elliptic_curve(i.to_bigint()?, j.to_bigint()?, p.to_bigint()?) {
-                points.push((i.to_bigint()?, j.to_bigint()?));
+            if in_elliptic_curve(Int::from(i), Int::from(j), Int::from(p)) {
+                points.push((Int::from(i), Int::from(j)));
             }
         }
     }
 
-    let a = BigInt::parse_bytes(C1, 10).unwrap();
-    let b = BigInt::parse_bytes(C2, 10).unwrap();
-    println!("y^2 = x^3 + {} * x + {} (mod {})", a % p.to_bigint().unwrap(), b % p.to_bigint().unwrap(), p);
+    let a = Int::from_str_radix(C1, 10).unwrap();
+    let b = Int::from_str_radix(C2, 10).unwrap();
+    println!("y^2 = x^3 + {} * x + {} (mod {})", a % Int::from(p), b % Int::from(p), p);
 
     println!("Curve order = {}", &points.len());
     // println!("{:?}", points);
     let point = &points[0];
-    println!("Point order {:?}: {:?}", point, point_order(point, p.to_bigint().unwrap()));
+    // println!("Point order {:?}: {:?}", point, point_order(point, Int::from(p)));
     println!("Time elapsed is: {:?}", start.elapsed());
-    Ok(())
 }
