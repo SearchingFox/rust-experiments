0beca19cda893ebee9f9f71d8e3664fe32905d7c
SearchingFox <n3tstrang3r@yandex.ru>
2021-08-27 18:40:44 +0300

    Refactor main.rs and formatted with rust-fmt

--- notes-graph/src/main.rs
+++ notes-graph/src/main.rs
@@ -2,15 +2,16 @@ use rand::Rng;
 use regex::Regex;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use std::collections::HashMap;
 
-const WINDOW_WIDTH:  u32 = 1920;
+const WINDOW_WIDTH: u32 = 1920;
 const WINDOW_HEIGHT: u32 = 1080;
 const BACKGROUND_COLOR: Color = Color::RGB(45, 45, 45);
 const FOREGROUND_COLOR: Color = Color::RGB(194, 194, 194);
-const SIZE: u32 = 10;
+const SIZE: u32 = 16;
 
 fn get_notes(folder_path: &str) -> (HashMap<String, Rect>, HashMap<String, Vec<String>>) {
     let mut rng = rand::thread_rng();
@@ -22,13 +23,18 @@ fn get_notes(folder_path: &str) -> (HashMap<String, Rect>, HashMap<String, Vec<S
         let os_str = file.file_name();
         let file_name = os_str.to_str().unwrap();
 
-        note_point.insert("[[".to_string() + &file_name[0..file_name.len()-3] + "]]"
-                        , Rect::new(rng.gen_range(10..1900), rng.gen_range(10..1040), SIZE, SIZE));
-        note_notes.insert("[[".to_string() + &file_name[0..file_name.len()-3] + "]]"
-                        , Regex::new(r"\[\[.*\]\]").unwrap()
-                            .find_iter(&std::fs::read_to_string(file.path()).unwrap())
-                            .map(|x| x.as_str().to_string())
-                            .collect::<Vec<_>>());
+        note_point.insert(
+            "[[".to_string() + &file_name[0..file_name.len() - 3] + "]]",
+            Rect::new(rng.gen_range(10..1900), rng.gen_range(10..1040), SIZE, SIZE),
+        );
+        note_notes.insert(
+            "[[".to_string() + &file_name[0..file_name.len() - 3] + "]]",
+            Regex::new(r"\[\[.*\]\]")
+                .unwrap()
+                .find_iter(&std::fs::read_to_string(file.path()).unwrap())
+                .map(|x| x.as_str().to_string())
+                .collect::<Vec<_>>(),
+        );
     }
 
     (note_point, note_notes)
@@ -36,20 +42,56 @@ fn get_notes(folder_path: &str) -> (HashMap<String, Rect>, HashMap<String, Vec<S
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sdl_context = sdl2::init()?;
-    let mut canvas = sdl_context.video()?
-        .window("ObsVis", WINDOW_WIDTH, WINDOW_WIDTH)
-        .maximized().build()?
-        .into_canvas().build()?;
-    
-    let (note_point, note_notes) = get_notes("D:/ObsLogseq/Data");
+    let mut canvas = sdl_context
+        .video()?
+        .window("ObsVis", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .maximized()
+        .build()?
+        .into_canvas()
+        .build()?;
     let mut event_pump = sdl_context.event_pump()?;
+
+    let (mut note_point, note_notes) = get_notes("D:/ObsLogseq/Data");
+    let mut hit_node_name = "".to_string();
+
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                Event::MouseButtonDown {
+                    mouse_btn: MouseButton::Left,
+                    x,
+                    y,
+                    ..
+                } => {
+                    for (n, p) in &note_point {
+                        if p.contains_point(Point::new(x, y)) {
+                            hit_node_name = n.to_string();
+                        }
+                    }
+                }
+                Event::MouseButtonUp {
+                    mouse_btn: MouseButton::Left,
+                    ..
+                } => {
+                    if !hit_node_name.is_empty() {
+                        hit_node_name.clear();
+                    }
+                }
+                Event::MouseMotion {
+                    mousestate, x, y, ..
+                } => {
+                    if mousestate.left() && !hit_node_name.is_empty() {
+                        note_point
+                            .entry(hit_node_name.to_string())
+                            .or_insert(Rect::new(0, 0, 0, 0))
+                            .center_on(Point::new(x, y));
+                    }
+                }
                 _ => {}
             }
         }
@@ -59,15 +101,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         canvas.set_draw_color(FOREGROUND_COLOR);
 
         canvas.draw_rects(&note_point.values().cloned().collect::<Vec<_>>())?;
-        for (k, v) in &note_notes {
-            for j in v {
-                match note_point.get(j) {
-                    Some(end) => {
-                        let start = note_point[k];
-                        canvas.draw_line( Point::new(start.x+((SIZE/2) as i32), start.y+((SIZE/2) as i32))
-                                        , Point::new(end.x+((SIZE/2) as i32)  , end.y+((SIZE/2) as i32))
-                            ).unwrap();
-                        },
+        for (note, notes) in &note_notes {
+            for point in notes {
+                match note_point.get(point) {
+                    Some(end_point) => {
+                        canvas.draw_line(note_point[note].center(), end_point.center())?;
+                    }
                     None => {}
                 }
             }
@@ -75,7 +114,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         canvas.present();
 
-        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000 / 20));
+        std::thread::sleep(std::time::Duration::new(0, 1_000_000_000 / 120));
     }
 
     Ok(())
