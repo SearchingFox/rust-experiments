64c44d76872345f9bd83fef0ab2d245c3cc3696f
SearchingFox <n3tstrang3r@yandex.ru>
2021-08-28 17:24:36 +0300

    Add rendering textures with nodes' names and some refactoring

--- notes-graph/src/main.rs
+++ notes-graph/src/main.rs
@@ -5,54 +5,71 @@ use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
-use std::collections::HashMap;
 
 const WINDOW_WIDTH: u32 = 1920;
 const WINDOW_HEIGHT: u32 = 1080;
 const BACKGROUND_COLOR: Color = Color::RGB(45, 45, 45);
-const FOREGROUND_COLOR: Color = Color::RGB(194, 194, 194);
-const SIZE: u32 = 16;
+const FOREGROUND_COLOR: Color = Color::RGB(200, 200, 200);
+const SIZE: u32 = 18;
 
-fn get_notes(folder_path: &str) -> (HashMap<String, Rect>, HashMap<String, Vec<String>>) {
+fn get_notes(folder_path: &str) -> (Vec<String>, Vec<Rect>, Vec<Vec<String>>) {
     let mut rng = rand::thread_rng();
-    let mut note_point = HashMap::new();
-    let mut note_notes = HashMap::new();
+    let mut names = Vec::new();
+    let mut rects = Vec::new();
+    let mut links = Vec::new();
 
-    for f in std::fs::read_dir(folder_path).unwrap() {
-        let file = f.unwrap();
-        let os_str = file.file_name();
-        let file_name = os_str.to_str().unwrap();
+    for f in std::fs::read_dir(folder_path).unwrap().take(70) {
+        let file_path = f.unwrap().path();
+        let name = file_path.file_stem().unwrap().to_str().unwrap().to_string();
 
-        note_point.insert(
-            "[[".to_string() + &file_name[0..file_name.len() - 3] + "]]",
-            Rect::new(rng.gen_range(10..1900), rng.gen_range(10..1040), SIZE, SIZE),
-        );
-        note_notes.insert(
-            "[[".to_string() + &file_name[0..file_name.len() - 3] + "]]",
+        names.push(name);
+        rects.push(Rect::new(
+            rng.gen_range(10..1900),
+            rng.gen_range(10..1040),
+            SIZE,
+            SIZE,
+        ));
+        links.push(
             Regex::new(r"\[\[.*\]\]")
                 .unwrap()
-                .find_iter(&std::fs::read_to_string(file.path()).unwrap())
-                .map(|x| x.as_str().to_string())
+                .find_iter(&std::fs::read_to_string(file_path).unwrap())
+                .map(|x| {
+                    let t = x.as_str();
+                    t[2..t.len() - 2].to_string()
+                })
                 .collect::<Vec<_>>(),
         );
     }
 
-    (note_point, note_notes)
+    (names, rects, links)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sdl_context = sdl2::init()?;
+    let mut event_pump = sdl_context.event_pump()?;
     let mut canvas = sdl_context
         .video()?
         .window("ObsVis", WINDOW_WIDTH, WINDOW_HEIGHT)
         .maximized()
+        .vulkan()
         .build()?
         .into_canvas()
         .build()?;
-    let mut event_pump = sdl_context.event_pump()?;
+    let texture_creator = canvas.texture_creator();
 
-    let (mut note_point, note_notes) = get_notes("D:/ObsLogseq/Data");
-    let mut hit_node_name = "".to_string();
+    let ttf_context = sdl2::ttf::init()?;
+    let font = ttf_context.load_font("FiraCode-Regular.ttf", 24)?;
+
+    let mut hit_node: Option<usize> = None;
+    let (names, mut rects, links) = get_notes("Data");
+    let textures = names
+        .iter()
+        .map(|name| {
+            texture_creator
+                .create_texture_from_surface(&font.render(&name).blended(FOREGROUND_COLOR).unwrap())
+                .unwrap() // blended is slow but nice looking
+        })
+        .collect::<Vec<_>>();
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -68,28 +85,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     y,
                     ..
                 } => {
-                    for (n, p) in &note_point {
-                        if p.contains_point(Point::new(x, y)) {
-                            hit_node_name = n.to_string();
-                        }
-                    }
-                }
-                Event::MouseButtonUp {
-                    mouse_btn: MouseButton::Left,
-                    ..
-                } => {
-                    if !hit_node_name.is_empty() {
-                        hit_node_name.clear();
-                    }
+                    hit_node = rects
+                        .iter()
+                        .position(|rect| rect.contains_point(Point::new(x, y)));
                 }
                 Event::MouseMotion {
                     mousestate, x, y, ..
                 } => {
-                    if mousestate.left() && !hit_node_name.is_empty() {
-                        note_point
-                            .entry(hit_node_name.to_string())
-                            .or_insert(Rect::new(0, 0, 0, 0))
-                            .center_on(Point::new(x, y));
+                    if mousestate.left() {
+                        match hit_node {
+                            Some(i) => rects[i].center_on(Point::new(x, y)),
+                            None => {}
+                        }
                     }
                 }
                 _ => {}
@@ -100,13 +107,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         canvas.clear();
         canvas.set_draw_color(FOREGROUND_COLOR);
 
-        canvas.draw_rects(&note_point.values().cloned().collect::<Vec<_>>())?;
-        for (note, notes) in &note_notes {
-            for point in notes {
-                match note_point.get(point) {
-                    Some(end_point) => {
-                        canvas.draw_line(note_point[note].center(), end_point.center())?;
-                    }
+        // Nodes
+        for i in 0..names.len() {
+            canvas.fill_rect(Some(rects[i]))?;
+
+            let sdl2::render::TextureQuery { width, height, .. } = textures[i].query();
+            canvas.copy(
+                &textures[i],
+                None,
+                Some(Rect::from_center(
+                    rects[i].center().offset(0, -28),
+                    width,
+                    height,
+                )),
+            )?;
+        }
+
+        // Edges
+        for i in 0..names.len() {
+            for link_name in &links[i] {
+                match names.iter().position(|name| name == link_name) {
+                    Some(end) => canvas.draw_line(rects[i].center(), rects[end].center())?,
                     None => {}
                 }
             }
