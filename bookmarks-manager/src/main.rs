3fa297fd78534a475ec9ddf79c77edc3dcc5653a
SearchingFox <n3tstrang3r@yandex.ru>
2022-11-18 04:20:34 +1000

    Refactor bookmarks-manager

--- bookmarks-manager/src/main.rs
+++ bookmarks-manager/src/main.rs
@@ -1,4 +1,5 @@
 #![feature(path_file_prefix)]
+#![feature(iter_array_chunks)]
 
 use std::collections::{HashMap, HashSet};
 use std::fs;
@@ -56,13 +57,23 @@ impl Ord for Bookmark {
     }
 }
 
+fn write_to_file(file_name: &Path, content: &Bookmarks) -> Result<(), std::io::Error> {
+    fs::write(
+        file_name,
+        content
+            .into_iter()
+            .map(|b| b.to_string(true))
+            .collect::<Vec<_>>()
+            .join("\n"),
+    )
+}
+
 fn get_from_txt(file_path: &Path) -> Bookmarks {
     fs::read_to_string(file_path)
         .unwrap()
         .lines()
-        .collect::<Vec<_>>()
-        .chunks_exact(2)
-        .map(|x| Bookmark {
+        .array_chunks()
+        .map(|x: [&str; 2]| Bookmark {
             title: x[0].to_string(),
             url: x[1].to_string(),
         })
@@ -71,18 +82,13 @@ fn get_from_txt(file_path: &Path) -> Bookmarks {
 
 fn get_from_html(file_path: &Path) -> Bookmarks {
     fs::read_to_string(file_path)
-        .unwrap()
+        .expect("Couldn't read html file")
         .lines()
         .filter_map(|line| {
-            if line.trim_start().starts_with("<DT><A") {
-                let title =
-                    line[line.find("\">").unwrap() + 2..line.find("</").unwrap()].to_string();
-                let url =
-                    line[line.find("HREF=\"").unwrap() + 6..line.find("\" ").unwrap()].to_string();
-                Some(Bookmark { title, url })
-            } else {
-                None
-            }
+            line.trim_start().starts_with("<DT><A").then(|| Bookmark {
+                title: line[line.find("\">").unwrap() + 2..line.find("</").unwrap()].to_string(),
+                url: line[line.find("HREF=\"").unwrap() + 6..line.find("\" ").unwrap()].to_string(),
+            })
         })
         .collect()
 }
@@ -93,16 +99,18 @@ fn get_from_md(file_path: &Path) -> Bookmarks {
         .lines()
         .map(|x| {
             let splitp = x.rfind("]").unwrap();
-            let title = x[x.find("[").unwrap() + 1..splitp].to_string();
-            let url = x[splitp + 2..x.rfind(")").unwrap()].to_string();
-            Bookmark { title, url }
+            Bookmark {
+                title: x[x.find("[").unwrap() + 1..splitp].to_string(),
+                url: x[splitp + 2..x.rfind(")").unwrap()].to_string(),
+            }
         })
         .collect()
 }
 
 fn get_from_json(file_path: &Path) -> Bookmarks {
-    let res: Vec<JsonFile> = from_str(&fs::read_to_string(file_path).expect("Couldn't read file"))
-        .expect("Couldn't parse json file");
+    let res: Vec<JsonFile> =
+        from_str(&fs::read_to_string(file_path).expect("Couldn't read json file"))
+            .expect("Couldn't parse json file");
     res.into_iter()
         .flat_map(|x| x.windows.into_values().flat_map(|y| y.into_values()))
         .collect()
@@ -130,29 +138,19 @@ fn main() {
     match args[1].as_str() {
         "--htt" => {
             for i in &args[2..] {
-                let p = Path::new(i);
-                fs::write(
-                    p.with_extension("md"),
-                    get_from_file(&p)
-                        .into_iter()
-                        .map(|x| x.to_string(true))
-                        .collect::<Vec<_>>()
-                        .join("\n"),
+                write_to_file(
+                    &Path::new(i).with_extension("md"),
+                    &get_from_file(Path::new(i)),
                 )
                 .expect("Couldn't write output file");
             }
         }
         "--uni" => {
-            let mut res: Bookmarks = HashSet::new();
-            for i in &args[2..] {
-                res.extend(get_from_file(Path::new(&i)));
-            }
-            fs::write(
-                "uni.txt",
-                res.into_iter()
-                    .map(|x| x.to_string(true))
-                    .collect::<Vec<_>>()
-                    .join("\n"),
+            write_to_file(
+                &Path::new(&args[2]).with_file_name("united_rusted.md"),
+                &args[2..].into_iter().fold(HashSet::new(), |acc, hs| {
+                    acc.union(&get_from_file(Path::new(&hs))).cloned().collect()
+                }),
             )
             .expect("Couldn't write output file");
         }
@@ -164,20 +162,16 @@ fn main() {
                 .expect("Couldn't parse chunk size");
             let mut inp: Vec<Bookmark> = get_from_file(file_path).into_iter().collect();
             inp.sort();
+            let old_file_name = file_path
+                .file_prefix()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string();
 
             for (i, el) in inp.chunks(chunk_size).enumerate() {
                 fs::write(
-                    file_path.with_file_name(
-                        file_path
-                            .file_prefix()
-                            .unwrap()
-                            .to_str()
-                            .unwrap()
-                            .to_string()
-                            + "_"
-                            + &(i + 1).to_string()
-                            + ".md",
-                    ),
+                    file_path.with_file_name(old_file_name + "_" + &(i + 1).to_string() + ".md"),
                     el.into_iter()
                         .map(|x| x.to_string(true))
                         .collect::<Vec<_>>()
@@ -186,44 +180,29 @@ fn main() {
                 .expect("Couldn't write output file");
             }
         }
-        "--dedup" => {
-            let file_path = Path::new(&args[2]);
-            let mut all: HashSet<Bookmark> = HashSet::new();
-
-            for folder_path in &args[3..] {
-                all.extend(get_from_folder(Path::new(folder_path)));
-            }
-
-            let out = get_from_file(file_path)
-                .difference(&all)
-                .into_iter()
-                .map(|x| x.to_string(true))
-                .collect::<Vec<_>>();
-            fs::write("C:/Users/Asus/Desktop/out--dedup.txt", out.join("\n"))
-                .expect("Couldn't write to file");
-            println!("{}", out.len());
-        }
-        _ => {
-            // let format = &args[2];
-            let source_path = Path::new(&args[3]);
+        "--ddp" => {
+            let source_path = Path::new(&args[2]);
             let source = get_from_file(source_path);
             let l = source.len();
 
-            let all_urls: Bookmarks = HashSet::new();
+            let mut all_urls: Bookmarks = HashSet::new();
+            for folder_path in &args[3..] {
+                all_urls.extend(get_from_folder(Path::new(folder_path)));
+            }
 
             let result: Bookmarks = source.difference(&all_urls).cloned().collect();
             println!("{} links are not in bookmarks", result.len());
 
             if !result.is_empty() && result.len() < l {
                 fs::write(
-                    source_path.parent().unwrap().join(
+                    source_path.with_file_name(
                         source_path
                             .file_stem()
                             .unwrap()
                             .to_str()
                             .unwrap()
                             .to_string()
-                            + "_uniq_rusted.txt",
+                            + "_uniq_rusted.md",
                     ),
                     result
                         .into_iter()
@@ -234,5 +213,6 @@ fn main() {
                 .expect("Write to output file failed");
             }
         }
+        _ => {}
     }
 }
